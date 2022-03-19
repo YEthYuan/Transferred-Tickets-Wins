b1e8883b2dc80380d986115f3f7fffd91e92bbf3
Maxwell-Yuan <2634643804@qq.com>
2022-03-19 17:01:44 +0800

    upload scripts

--- omp/main.py
+++ omp/main.py
@@ -30,7 +30,7 @@ parser.add_argument('--attack-lr', type=str, help='step size for PGD', default='
 # Custom arguments
 parser.add_argument('--dataset', type=str, default='cifar10',
                     help='Dataset (Overrides the one in robustness.defaults)')
-parser.add_argument('--data', type=str, default='../../data')
+parser.add_argument('--data', type=str, default='/home/yuanye/data')
 parser.add_argument('--out-dir', type=str, default='runs')
 parser.add_argument('--exp-name', type=str, default='cifar10-transfer-prune')
 parser.add_argument('--arch', type=str, default='resnet50')
@@ -91,9 +91,9 @@ def main(args, store):
     print("L1 Unstructured Pruning Start")
     pruning_model(model, cur_prune_rate)
     print("Pruning Done!")
-    # remove_prune(model)
-    check_sparsity(model, use_mask=False)
+    # check_sparsity(model, use_mask=True)
     remove_prune(model)
+    check_sparsity(model, use_mask=False)
 
     # update_params = freeze_model(model, freeze_level=args.freeze_level)
     update_params = None
---
+++ omp/scripts/img-c10-resnet18-eps3-p40-finetune.sh
@@ -0,0 +1,16 @@
+python main.py \
+  --arch resnet18 \
+  --eps 3 \
+  --prune_rate 0.4 \
+  --dataset cifar10 \
+  --exp-name img-c10-resnet18-eps3-p40-finetune \
+  --epochs 150 \
+  --lr 0.01 \
+  --step-lr 30 \
+  --batch-size 64 \
+  --weight-decay 5e-4 \
+  --adv-train 0 \
+  --adv-eval 1 \
+  --workers 0 \
+  --model-path pretrained_models/resnet18_l2_eps3.ckpt \
+  --freeze-level -1
