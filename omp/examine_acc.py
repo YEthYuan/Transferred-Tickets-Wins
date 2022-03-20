0748cea13f057cb068a71dc80141479e045c1600
Ye Yuan <2634643804@qq.com>
2022-03-20 22:29:29 +0800

    adjust settings

--- omp/examine_acc.py
+++ omp/examine_acc.py
@@ -3,7 +3,7 @@ import torchvision.models as models
 import numpy as np
 
 model = models.resnet18(pretrained=False)
-file_path = 'runs/img-c10-resnet18-eps0-p90-finetune/checkpoint.pt.best'
+file_path = 'runs/img-c10-resnet18-eps0-p40-finetune/checkpoint.pt.best'
 state_dict = torch.load(file_path)
 model.load_state_dict(state_dict['model'])
 print(state_dict)
