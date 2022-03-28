74a22ae4370ca0ac388f864d46c63475d30399df
Maxwell-Yuan <2634643804@qq.com>
2022-03-28 21:26:56 +0800

    downstream evaluation code ready

--- imp/main_eval_downstream.py
+++ imp/main_eval_downstream.py
@@ -1,151 +1,367 @@
-'''
-load lottery tickets and evaluation 
+"""
+load lottery tickets and evaluation
 support datasets: cifar10, Fashionmnist, cifar100
-'''
+"""
 
+import argparse
+import logging
 import os
-import pdb
-import time 
-import pickle
+import pathlib
 import random
 import shutil
-import argparse
-import numpy as np  
-from copy import deepcopy
-import matplotlib.pyplot as plt
+import time
+import warnings
+import copy
+import math
+
+import importlib
+
+import numpy as np
 import torch
-import torch.optim
 import torch.nn as nn
-import torch.utils.data
-import torch.nn.functional as F
-import torchvision.models as models
+import torch.nn.parallel
 import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from torch.utils.data.sampler import SubsetRandomSampler
-from utils import *
+import torchvision.models as models
+from matplotlib import pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from collections import OrderedDict
+
 from pruning_utils import *
+from dataset import *
+
+model_names = sorted(name for name in models.__dict__
+                     if name.islower() and not name.startswith("__")
+                     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch Evaluation Tickets')
+############################# required settings ################################
+parser.add_argument('--data', metavar='DIR', default='/home/yuanye/data',
+                    help='path to dataset')
+parser.add_argument('--set', type=str, default='cifar10', help='ImageNet, cifar10, cifar100, svhn')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                    choices=model_names,
+                    help='model architecture: ' +
+                         ' | '.join(model_names) +
+                         ' (default: resnet50)')
+parser.add_argument('--epochs', default=150, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('-b', '--batch-size', default=64, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--decreasing_lr', default='50,100', help='decreasing strategy')
+parser.add_argument('--log_dir', default='runs', type=str)
+parser.add_argument('--name', default='R18_Linf_Eps2_s80_c10', type=str, help='experiment name')
+parser.add_argument('--weight_dir', type=str, default='/home/yf22/ResNet_ckpt/resnet18_linf_eps2.0.ckpt',
+                    help='path of the pretrained weight')
+parser.add_argument('--mask_dir', type=str, default='/home/yf22/ResNet_ckpt/resnet18_linf_eps2.0.ckpt',
+                    help='path of the extracted mask')
+parser.add_argument('--pytorch-pretrained', action='store_true',
+                    help='If True, loads a Pytorch pretrained natural weight.')
+parser.add_argument('--random', action="store_true", help="using random-init model")
+parser.add_argument("--trainer", type=str, default="default", help="cs, ss, or standard training")
 
-##################################### data setting #################################################
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--dataset', type=str, default='cifar10', help='dataset[cifar10&100, svhn, fmnist')
+############################# other settings ################################
+parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=5e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=50, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--adv-eval', action='store_true', default=False)
+parser.add_argument('--save-model', action='store_true', help="Save the finetuned model", default=True)
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--seed', default=142, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--gpu', default=None, type=int,
+                    help='GPU id to use.')
 
-##################################### model setting #################################################
-parser.add_argument('--arch', type=str, default='resnet50', help='model architecture[resnet18, resnet50, resnet152]')
-
-##################################### basic setting #################################################
-parser.add_argument('--seed', default=None, type=int, help='random seed')
-parser.add_argument('--save_dir', help='The directory used to save the trained models', default=None, type=str)
-parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--save_model', action="store_true", help="whether saving model")
-parser.add_argument('--print_freq', default=50, type=int, help='print frequency')
-
-##################################### training setting #################################################
-parser.add_argument('--batch_size', type=int, default=128, help='batch size')
-parser.add_argument('--lr', default=0.1, type=float, help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
-parser.add_argument('--weight_decay', default=2e-4, type=float, help='weight decay')
-parser.add_argument('--epochs', default=182, type=int, help='number of total epochs to run')
-parser.add_argument('--warmup', default=1, type=int, help='warm up epochs')
-parser.add_argument('--decreasing_lr', default='91,136', help='decreasing strategy')
-
-##################################### Pruning setting #################################################
-parser.add_argument('--pretrained', default=None, type=str, help='pretrained weight of Ticket')
-parser.add_argument('--dict_key', default=None, type=str, help='key of pretrained file')
-parser.add_argument('--mask_dir', default=None, type=str, help='mask direction of Ticket')
-parser.add_argument('--conv1', action="store_true", help="whether prune conv1")
-parser.add_argument('--load_all', action="store_true", help="whether loading all weight in pretrained model")
-parser.add_argument('--reverse_mask', action="store_true", help="whether using reverse mask")
 
 def main():
-
-    best_sa = 0
     args = parser.parse_args()
-    print(args)
 
-    print('*'*50)
-    print('Dataset: {}'.format(args.dataset))
-    print('Model: {}'.format(args.arch))
-    print('*'*50)     
+    os.makedirs(args.log_dir, exist_ok=True)
 
-    torch.cuda.set_device(int(args.gpu))
-    os.makedirs(args.save_dir, exist_ok=True)
-    if args.seed:
-        setup_seed(args.seed)
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
 
-    # prepare dataset 
-    model, train_loader, val_loader, test_loader = setup_model_dataset(args)
-    model.cuda()
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
 
-    #loading tickets
-    load_ticket(model, args)
+    main_worker(args.gpu, args)
 
-    criterion = nn.CrossEntropyLoss()
+
+def main_worker(gpu, args):
+    if args.pytorch_pretrained:
+        args.weight_dir = None
+
+    run_base_dir, ckpt_base_dir, log_base_dir = get_directories(args)
+    args.ckpt_base_dir = ckpt_base_dir
+    os.makedirs(args.ckpt_base_dir, exist_ok=True)
+
+    log = logging.getLogger(__name__)
+    log_path = os.path.join(run_base_dir, 'log.txt')
+    handlers = [logging.FileHandler(log_path, mode='a+'),
+                logging.StreamHandler()]
+    logging.basicConfig(
+        format='[%(asctime)s] - %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S',
+        level=logging.INFO,
+        handlers=handlers)
+    log.info(args)
+
+    args.gpu = gpu
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+        log.info("[INIT] Use GPU: {} for training".format(args.gpu))
+
+    # create model with official pretrained weight or random initialization
+    print("=> using model '{}', dataset '{}'".format(args.arch, args.set))
+    log.info("[INIT] => using model '{}', dataset '{}'".format(args.arch, args.set))
+    model, train_loader, val_loader = get_model_dataset(args)
+    if_pruned = False
+
+    writer = SummaryWriter(log_dir=log_base_dir)
+
+    # Load pretrained weights
+    if args.weight_dir:
+        log.info("Loading pretrained weights")
+        print("=> loading checkpoint '{}'".format(args.weight_dir))
+        log.info("[LOAD] => loading checkpoint '{}'".format(args.weight_dir))
+        checkpoint = torch.load(args.weight_dir)
+
+        new_state_dict = OrderedDict()
+        sd = checkpoint['model']
+        for k, v in sd.items():
+            if 'attacker' in k:
+                break
+            if 'normalize' not in k:
+                name = k[len('module.model.'):]
+                new_state_dict[name] = v
+        model.load_state_dict(new_state_dict)
+        print("=> loaded checkpoint '{}' (epoch {})".format(args.model_path, checkpoint['epoch']))
+        log.info("[LOAD] => loaded checkpoint '{}' (epoch {})".format(args.model_path, checkpoint['epoch']))
+    else:
+        log.info("[LOAD] => Pytorch natural pretrained model")
+
+    if args.set != 'ImageNet':
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, args.classes)
+
+    print('dataparallel mode')
+    log.info("[INIT] dataparallel mode")
+    model = torch.nn.DataParallel(model).cuda()
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
     decreasing_lr = list(map(int, args.decreasing_lr.split(',')))
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=decreasing_lr, gamma=0.1)
 
+    # Load mask
+    print("=> loading mask '{}'".format(args.mask_dir))
+    log.info("[LOAD] => loading mask '{}'".format(args.mask_dir))
+    mask = torch.load(args.mask_dir)
+    prune_model_custom(model.module, mask['mask'], False)
+
+    # Get trainer and tester
+    train, validate, validate_adv, modifier = get_trainer(args)
+
+    cudnn.benchmark = True
+
     all_result = {}
     all_result['train'] = []
-    all_result['test_ta'] = []
-    all_result['ta'] = []
+    all_result['nat_acc'] = []
+    all_result['adv_acc'] = []
+    all_result['robustness_at_best_acc'] = 0.0
+    all_result['best_acc1'] = 0.0
+    all_result['best_epoch'] = 0
 
     start_epoch = 0
-    remain_weight = check_sparsity(model, conv1=args.conv1)
+    mask_sp = check_sparsity(model, use_mask=True, conv1=False)
+    log.info(f"[EVAL] Mask Sparsity: {mask_sp:.2f}")
+    all_result['sparsity'] = mask_sp
 
     for epoch in range(start_epoch, args.epochs):
-
+        log.info("-" * 150)
         print(optimizer.state_dict()['param_groups'][0]['lr'])
-        acc = train(train_loader, model, criterion, optimizer, epoch, args)
+        log.info(
+            f"[TRAIN] Epoch {epoch} start, lr {optimizer.state_dict()['param_groups'][0]['lr']}")
+
+        # train for one epoch
+        train_acc1, train_acc5 = train(train_loader, model, criterion, optimizer, epoch, args, writer)
+        log.info("[TRAIN] Train done! Train@1: %.2f, Train@5: %.2f", train_acc1, train_acc5)
+        all_result['train'].append(train_acc1)
 
         # evaluate on validation set
-        tacc = test(val_loader, model, criterion, args)
-        # evaluate on test set
-        test_tacc = test(test_loader, model, criterion, args)
+        nat_acc1, nat_acc5 = validate(val_loader, model, criterion, args, writer, epoch)
+        log.info("[EVAL] Natural eval done! Nat@1: %.2f, Nat@5: %.2f", nat_acc1, nat_acc5)
+        all_result['nat_acc'].append(nat_acc1)
+
+        if args.adv_eval:
+            # evaluate on adversary validation set
+            adv_acc1, adv_acc5 = validate_adv(val_loader, model, criterion, args, writer, epoch)
+            log.info("[EVAL] Robust eval done! Adv@1: %.2f, Adv@5: %.2f", adv_acc1, adv_acc5)
+        else:
+            adv_acc1, adv_acc5 = -1, -1
+        all_result['adv_acc'].append(adv_acc1)
+
+        # remember best acc@1 and save checkpoint
+        is_best = nat_acc1 > all_result['best_acc1']
+        all_result['best_acc1'] = max(nat_acc1, all_result['best_acc1'])
+
+        if is_best:
+            all_result['best_epoch'] = epoch + 1
+            if args.adv_eval:
+                all_result['robustness_at_best_acc'] = adv_acc1
+            else:
+                all_result['robustness_at_best_acc'] = -1
+            log.info("[EVAL] ***** This is the best epoch so far ***** ")
+
+        log.info("[EVAL] Best Accuracy: %.2f at epoch %d, robustness at best acc %.2f", all_result['best_acc1'],
+                 all_result['best_epoch'], all_result['robustness_at_best_acc'])
 
         scheduler.step()
 
-        all_result['train'].append(acc)
-        all_result['ta'].append(tacc)
-        all_result['test_ta'].append(test_tacc)
-        all_result['remain_weight'] = remain_weight
-
-        # remember best prec@1 and save checkpoint
-        is_best_sa = tacc  > best_sa
-        best_sa = max(tacc, best_sa)
-
         if args.save_model:
-
             save_checkpoint({
                 'result': all_result,
                 'epoch': epoch + 1,
                 'state_dict': model.state_dict(),
-                'best_sa': best_sa,
+                'best_acc1': all_result['best_acc1'],
                 'optimizer': optimizer.state_dict(),
                 'scheduler': scheduler.state_dict()
-            }, is_SA_best=is_best_sa, save_path=args.save_dir)
+            }, is_best=is_best, checkpoint=args.ckpt_base_dir)
 
         else:
             save_checkpoint({
                 'result': all_result
-            }, is_SA_best=False, save_path=args.save_dir)
+            }, is_best=False, checkpoint=args.ckpt_base_dir)
 
         plt.plot(all_result['train'], label='train_acc')
-        plt.plot(all_result['ta'], label='val_acc')
-        plt.plot(all_result['test_ta'], label='test_acc')
+        plt.plot(all_result['nat_acc'], label='natural_acc')
         plt.legend()
-        plt.savefig(os.path.join(args.save_dir, 'net_train.png'))
+        plt.savefig(os.path.join(log_base_dir, 'net_train.png'))
         plt.close()
 
-    check_sparsity(model, conv1=args.conv1)
-    print('* best SA={}'.format(all_result['test_ta'][np.argmax(np.array(all_result['ta']))]))
+        if args.adv_eval:
+            plt.plot(all_result['adv_acc'], label='robust_acc')
+            plt.legend()
+            plt.savefig(os.path.join(log_base_dir, 'robustness.png'))
+            plt.close()
+
+    check_sparsity(model, use_mask=True, conv1=False)
+    log.info(all_result)
+
+
+def save_checkpoint(state, is_best, checkpoint, filename='checkpoint.pth.tar', best_name='model_best.pth.tar'):
+    filepath = os.path.join(checkpoint, filename)
+    torch.save(state, filepath)
+
+    if is_best:
+        bestpath = os.path.join(checkpoint, best_name)
+        shutil.copyfile(filepath, bestpath)
+        return bestpath
+
+    return filepath
+
+
+def get_trainer(args):
+    print(f"=> Using trainer from trainers.{args.trainer}")
+    trainer = importlib.import_module(f"trainers.{args.trainer}")
+
+    if args.attack_type == 'None':
+        return trainer.train, trainer.validate, trainer.validate_adv, trainer.modifier
+    else:
+        if args.attack_type == 'free':
+            return trainer.train_adv_free, trainer.validate, trainer.validate_adv, trainer.modifier
+        else:
+            return trainer.train_adv, trainer.validate, trainer.validate_adv, trainer.modifier
+
+
+def get_directories(args):
+    if args.log_dir is None:
+        run_base_dir = pathlib.Path(
+            f"runs/{args.name}"
+        )
+    else:
+        run_base_dir = pathlib.Path(
+            f"{args.log_dir}/{args.name}"
+        )
+
+    # if _run_dir_exists(run_base_dir):
+    #     rep_count = 0
+    #     while _run_dir_exists(run_base_dir / str(rep_count)):
+    #         rep_count += 1
+
+    #     run_base_dir = run_base_dir / str(rep_count)
+
+    log_base_dir = run_base_dir / "logs"
+    ckpt_base_dir = run_base_dir / "checkpoints"
+
+    if not run_base_dir.exists():
+        os.makedirs(run_base_dir)
+    if not log_base_dir.exists():
+        os.makedirs(log_base_dir)
+    if not ckpt_base_dir.exists():
+        os.makedirs(ckpt_base_dir)
+
+    (run_base_dir / "settings.txt").write_text(str(args))
+
+    return run_base_dir, ckpt_base_dir, log_base_dir
+
+
+def get_model_dataset(args):
+    # prepare dataset
+    if args.set == 'cifar10':
+        args.classes = 10
+        train_loader, _, test_loader = cifar10_dataloaders(args, use_val=False)
+    elif args.set == 'cifar100':
+        args.classes = 100
+        train_loader, _, test_loader = cifar100_dataloaders(args, use_val=False)
+    elif args.set == 'svhn':
+        args.classes = 10
+        train_loader, _, test_loader = svhn_dataloaders(args, use_val=False)
+    elif args.set == 'fmnist':
+        args.classes = 10
+        train_loader, _, test_loader = fashionmnist_dataloaders(args, use_val=False)
+    elif args.set == 'ImageNet':
+        args.classes = 1000
+        train_loader, _, test_loader = imagenet_dataloaders(args, use_val=False)
+    else:
+        raise ValueError("Unknown Dataset")
+
+    # prepare model
+    model = models.__dict__[args.arch](pretrained=(not args.random))
+
+    return model, train_loader, test_loader
 
 
 if __name__ == '__main__':
     main()
-
-
