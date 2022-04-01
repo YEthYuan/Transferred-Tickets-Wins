336e9b111d9b78e3778eb2f524cde1d54a8bba27
Windsao <swu23@gwu.edu>
2022-04-01 02:32:38 -0500

    modify script

--- imp/robsut.sh
+++ imp/robsut.sh
@@ -1,3 +1,4 @@
 export MIOPEN_USER_DB_PATH=/scratch/yf22/ 
 #HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main_imp.py
-HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main_imp.py --attack_type None --pytorch-pretrained --model-path ''
+#HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main_imp.py --attack_type None --pytorch-pretrained --model-path ''
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main_imp.py  --model-path '' --resume 'runs/R50_Linf_Eps2_2/checkpoints/checkpoint.pth.tar'
