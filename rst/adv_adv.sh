d15766ce541758be6f489992baece6a3dd2310b8
Windsao <sw99@rice.edu>
2022-04-05 15:00:06 -0500

    add script

--- rst/adv_adv.sh
+++ rst/adv_adv.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 export MIOPEN_USER_DB_PATH=/scratch/yf22/ 
-
-p=10
-until [ ! $p -lt 100 ]
-do
-  echo -e "\n\n"
-  echo "Now Pruning Rate ${p}%"
-  echo -e "\n\n"
-  sleep 1s
-  
-HIP_VISIBLE_DEVICES=4,5,6,7  python main.py \
+HIP_VISIBLE_DEVICES=0,1,2,3 python main.py \
+#HIP_VISIBLE_DEVICES=4,5,6,7 python main.py \
     --arch ResNet18 \
     --attack_type fgsm-rs \
-    --prune_percent ${p} \
+    --prune_percent $1 \
     --task search \
     --set ImageNet \
     --data /scratch/cl114/ILSVRC/Data/CLS-LOC/ \
-    --pytorch-pretrained  \
-    --name img-Linf2-nat_weight-adv_search \
+    --pretrained /home/yf22/ResNet_ckpt/resnet18_linf_eps2.0.ckpt \
+    --name img-Linf4-adv_weight-adv_search \
     --config config_rst/resnet18-ukn-unsigned-imagenet.yaml \
     --epochs 90 \
     --optimizer sgd \
@@ -32,6 +24,4 @@ HIP_VISIBLE_DEVICES=4,5,6,7  python main.py \
     --epsilon 2 \
     --alpha 2.5 \
     --attack_iters 1 \
-    --constraint Linf
-  p=`expr $p + 10`
-done
+    --constraint Linf > adv_2.txt 2>&1 &
